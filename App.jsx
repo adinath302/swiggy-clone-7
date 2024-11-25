aed0e44545a5e664615ff74b20786371dfc37ea9
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-11-25 21:50:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Category from "./components/category";
+import Toprest from "./components/Toprest";
+
+const App = () => {
+  return (
+    <div className="">
+      <Header />
+      <Category />
+      <Toprest/>
+    </div>
+  );
+};
+
+export default App;
